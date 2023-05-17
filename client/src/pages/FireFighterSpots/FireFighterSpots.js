5ffeeb605489ea321519e301dd75551f9e7aacbe
alfonsodelaguardia <alfonso.diaz@blocklack.com>
2023-05-17 17:06:44 +0200

    renaming components

--- client/src/pages/FireFighterSpots/FireFighterSpots.js
+++ client/src/pages/FireFighterSpots/FireFighterSpots.js
@@ -8,52 +8,58 @@ import "./FireFighterSpots.scss";
 const FireFighterSpots = () => {
   const [fireFighterSpots, setFireFighterSpots] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     setIsLoading(true);
     async function fetchFireFighterSpots() {
-      await axios.get("http://localhost:4000/").then((response) => {
-        if (response && response.data) {
-          setFireFighterSpots(response.data);
+      try {
+        const { data } = await axios.get("http://localhost:4000/");
+        if (data) {
+          setFireFighterSpots(data);
         }
-      });
-      setIsLoading(false);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setIsLoading(false);
+      }
     }
     fetchFireFighterSpots();
   }, []);
 
-  const fireFighterSpotsData = fireFighterSpots && fireFighterSpots["@graph"];
+  const fireFighterSpotsData = fireFighterSpots?.["@graph"];
 
   if (isLoading) {
     return <Spinner />;
   }
 
+  if (error) {
+    return <div className="error-message">Error: {error.message}</div>;
+  }
+
   return (
     <Card>
       <div className="parking-spots">
-        {fireFighterSpotsData &&
-          fireFighterSpotsData.map((fireFighterSpot) => (
-            <div key={fireFighterSpot.id} className="parking-spots__data">
-              <div className="parking-spots__details">
-                <h4 className="parking-spots__title">
-                  {fireFighterSpot.title}
-                </h4>
-                <p>Localidad: {fireFighterSpot.address.locality}</p>
-                <p>Código Postal: {fireFighterSpot.address["postal-code"]}</p>
-                <p>
-                  Dirección de Aparcamiento:{" "}
-                  {fireFighterSpot.address["street-address"]}
-                </p>
-              </div>
-              <button className="parking-spots__button">
-                <a
-                  href={`https://www.google.com/maps/place/${fireFighterSpot.address["street-address"]}`}
-                >
-                  CÓMO LLEGAR
-                </a>
-              </button>
+        {fireFighterSpotsData?.map((fireFighterSpot) => (
+          <div key={fireFighterSpot.id} className="parking-spots__data">
+            <div className="parking-spots__details">
+              <h4 className="parking-spots__title">{fireFighterSpot?.title}</h4>
+              <p>Localidad: {fireFighterSpot?.address?.locality}</p>
+              <p>Código Postal: {fireFighterSpot?.address?.["postal-code"]}</p>
+              <p>
+                Dirección de Aparcamiento:{" "}
+                {fireFighterSpot?.address?.["street-address"]}
+              </p>
             </div>
-          ))}
+            <button className="parking-spots__button">
+              <a
+                href={`https://www.google.com/maps/place/${fireFighterSpot?.address?.["street-address"]}`}
+              >
+                CÓMO LLEGAR
+              </a>
+            </button>
+          </div>
+        ))}
       </div>
     </Card>
   );
